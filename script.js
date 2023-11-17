32de4214c005b12fbb7b7c261ba70c76bb4fcff9
Erick Santos <ericksantos2004.ep@gmail.com>
2023-11-17 04:11:22 -0300

    Alterações e melhorias

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
-const task = document.querySelector("#tasks");
-const btn = document.querySelector("#btn");
-const list = document.querySelector("#list");
-const span = document.querySelector("span")
+document.addEventListener("DOMContentLoaded", function() {
+    const taskInput = document.querySelector("#tasks");
+    const addButton = document.querySelector("#btn");
+    const taskList = document.querySelector("#list");
 
-btn.addEventListener("click", function(){
-    if(task.value == ""){
-        alert("Digite uma tarefa válida")
-    } 
-    else{
-        list.innerHTML += `<li>
-                                <ion-icon class="check icon" name="checkmark-circle-outline"></ion-icon>
-                                <span>${task.value}</span>
-                                <ion-icon name="create-outline" class="edit"></ion-icon>
-                                <ion-icon class="close" name="trash-outline"></ion-icon>
-                            </li>`
+    addButton.addEventListener("click", function() {
+        addTask(taskInput.value);
+        taskInput.value = "";
+    });
+
+    taskList.addEventListener("click", function(e) {
+        const targetElement = e.target;
+
+        if (targetElement.classList.contains("close")) {
+            removeTask(targetElement.parentElement);
+        } else if (targetElement.classList.contains("check")) {
+            toggleTaskCompletion(targetElement.parentElement);
+        } else if (targetElement.classList.contains("edit")) {
+            editTask(targetElement.parentElement);
+        }
+    });
+});
+
+function addTask(taskText) {
+    if (taskText.trim() === "") {
+        alert("Digite uma tarefa válida");
+        return;
     }
-    task.value = ""
 
+    const taskList = document.querySelector("#list");
+    const newTaskItem = document.createElement("li");
+    newTaskItem.innerHTML = `
+        <ion-icon class="check icon" name="checkmark-circle-outline"></ion-icon>
+        <span>${taskText}</span>
+        <ion-icon name="create-outline" class="edit"></ion-icon>
+        <ion-icon class="close" name="trash-outline"></ion-icon>
+    `;
 
-    const close = document.querySelectorAll(".close");
+    taskList.appendChild(newTaskItem);
+}
 
-    for(let i=0; i<close.length; i++){
-        close[i].addEventListener("click", function(){
-            close[i].parentElement.remove();
-        })
+function removeTask(taskElement) {
+    taskElement.remove();
+}
+
+function toggleTaskCompletion(taskElement) {
+    const checkIcon = taskElement.querySelector(".check");
+    const taskText = taskElement.querySelector("span");
+
+    if (checkIcon.style.color === "rgb(52, 146, 35)") {
+        // Se a tarefa estiver concluída, desmarcar
+        checkIcon.style.color = "";
+        taskText.style.textDecoration = "";
+    } else {
+        // Se a tarefa não estiver concluída, marcar como concluída
+        checkIcon.style.color = "#349223";
+        taskText.style.textDecoration = "line-through";
     }
+}
 
-    list.addEventListener("click",function(e){
-        e.target.parentElement.querySelector(".check").style.color = "#349223";
-        e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
-    })
+function editTask(taskElement) {
+    const taskText = taskElement.querySelector("span");
+    const newText = prompt("Editar tarefa:", taskText.textContent);
 
-
-})
+    if (newText !== null) {
+        taskText.textContent = newText;
+    }
+}
